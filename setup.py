821701542da055d566742d103f92546c96f20db4
Reiase <reiase@gmail.com>
2022-01-18 09:45:12 +0800

    release v0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hyperparameter",
-    version="0.0.1",
+    version="0.1.2",
     description="A hyper-parameter library for researchers, data scientists and machine learning engineers.
 ",
     long_description=long_description,
