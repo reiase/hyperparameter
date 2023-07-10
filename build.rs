1a836c2418dc346483ca36d330e498d6d3fe80e7
Reiase <reiase@gmail.com>
2023-07-10 09:57:53 +0800

    support soname for macos (#8)

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,librbackend.abi3.so")
+    let target = env::var("TARGET").unwrap();
+    if target.contains("linux") {
+        println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,librbackend.abi3.so")
+    }
+    if target.contains("apple") {
+        println!("cargo:rustc-cdylib-link-arg=-Wl,-install_name,librbackend.abi3.so")
+    }
 }
