a6e32fb83b6d9c8533dadbd1f8df8c4dddce2938
Reiase <reiase@gmail.com>
2024-06-16 17:55:31 +0800

    fix build problem when disable clap (#18)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,7 +7,6 @@ mod value;
 
 mod api;
 mod cfg;
-mod cli;
 mod ffi;
 mod xxh;
 
@@ -15,10 +14,15 @@ pub use crate::api::frozen;
 pub use crate::api::ParamScope;
 pub use crate::api::ParamScopeOps;
 pub use crate::cfg::AsParamScope;
-pub use crate::cli::generate_params_help;
-pub use crate::cli::PARAMS;
 pub use crate::storage::GetOrElse;
 pub use crate::storage::THREAD_STORAGE;
 pub use crate::value::Value;
 pub use crate::xxh::xxhash;
 pub use crate::xxh::XXHashable;
+
+#[cfg(feature = "clap")]
+mod cli;
+#[cfg(feature = "clap")]
+pub use crate::cli::generate_params_help;
+#[cfg(feature = "clap")]
+pub use crate::cli::PARAMS;
